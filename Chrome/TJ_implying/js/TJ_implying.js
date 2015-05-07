88ae9e0b8a9cfcca8079794a42d9bb8f0dbe16c0
Evgenii Vilkov <zzeneg@gmail.com>
2015-05-08 01:07:38 +0200

    Simplify script for Chrome

--- Chrome/TJ_implying/js/TJ_implying.js
+++ Chrome/TJ_implying/js/TJ_implying.js
@@ -1,14 +1,7 @@
-$(document).ready(function(){
-
-  $.each($(".commentWrapper .b-comment__text"), function(i,e){
-
-    $(e).find("p").html(function(j, oldHTML){
-
-      return oldHTML.replace(/(&gt;.*?)\<br\>/gm, "<span style='color: #789922 !important'>$1</span><br>");
-
+imply = function() {
+    [].forEach.call(document.querySelectorAll('.b-comment__text p'), function(el) {
+        el.innerHTML = el.innerHTML.replace(/(&gt;.*?)\<br\>/gm, "<span style='color: #789922 !important'>$1</span><br>");
     })
+}
 
-
-  });
-
-});
+document.readyState === 'loading' ? document.addEventListener('DOMContentLoaded', imply, false) : imply();
