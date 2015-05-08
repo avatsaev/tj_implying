1db5f7e67833eb640f9006ad9f59236f46f3dda4
A. Vatsaev <azero79@gmail.com>
2015-05-08 11:19:31 +0200

    updated regex and safari installer

--- Safari/TJ_implying.safariextension/js/TJ_implying.js
+++ Safari/TJ_implying.safariextension/js/TJ_implying.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
   $.each($(".commentWrapper .b-comment__text"), function(i,e){
 
-    $(e).find("p").append("<br>").html(function(j, oldHTML){
-      return oldHTML.replace(/(^&gt;.*?)\<br\>/gm, "<span style='color: #789922 !important'>$1</span><br>");
+    $(e).find("p").html(function(j, oldHTML){
+      return oldHTML.replace(/^(&gt;.*?)(\<br\>|$)/gm, "<span style='color: #789922 !important'>$1</span><br>");
     })
 
   });
