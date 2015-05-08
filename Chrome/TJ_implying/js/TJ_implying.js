2b196429d88354eca2652ff72bdf499c7a8b09f9
Evgenii Vilkov <zzeneg@gmail.com>
2015-05-08 10:55:38 +0200

    Update regex to detect start and end of comment

--- Chrome/TJ_implying/js/TJ_implying.js
+++ Chrome/TJ_implying/js/TJ_implying.js
@@ -1,6 +1,6 @@
 imply = function() {
     [].forEach.call(document.querySelectorAll('.b-comment__text p'), function(el) {
-        el.innerHTML = el.innerHTML.replace(/(&gt;.*?)\<br\>/gm, "<span style='color: #789922 !important'>$1</span><br>");
+        el.innerHTML = el.innerHTML.replace(/^(&gt;.*?)(\<br\>|$)/gm, "<span style='color: #789922 !important'>$1</span><br>");
     })
 }
 
