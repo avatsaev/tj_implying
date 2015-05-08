eb69caa69e2e5d108e7f39e7169a6b0d3d79f9bc
A. Vatsaev <azero79@gmail.com>
2015-05-08 10:28:24 +0200

    fixed single line citation bug

--- Safari/TJ_implying.safariextension/js/TJ_implying.js
+++ Safari/TJ_implying.safariextension/js/TJ_implying.js
@@ -2,12 +2,9 @@ $(document).ready(function(){
 
   $.each($(".commentWrapper .b-comment__text"), function(i,e){
 
-    $(e).find("p").html(function(j, oldHTML){
-
-      return oldHTML.replace(/(&gt;.*?)\<br\>/gm, "<span style='color: #789922 !important'>$1</span><br>");
-
+    $(e).find("p").append("<br>").html(function(j, oldHTML){
+      return oldHTML.replace(/(^&gt;.*?)\<br\>/gm, "<span style='color: #789922 !important'>$1</span><br>");
     })
-
 
   });
 
